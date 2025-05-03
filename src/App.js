83f4f547d4e93ee57cee84cbe0c9e79dfb9ca680
Ana Rostam <anna_rostam@yahoo.com>
2025-05-03 18:25:01 -0400

    added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header';
 import Home from './Pages/Home';
 import Footer from './Components/Footer';
